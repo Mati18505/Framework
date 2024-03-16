5c7d2a7d6ab31e9a8a7da89b434baf2c73485f95
tomson41.08 @gmail.com <mstulczewski@gmail.com>
2024-03-16 20:23:42 +0100

    Multiple windows event test.

--- Tests/Source/MultipleWindowsTest.h
+++ Tests/Source/MultipleWindowsTest.h
@@ -20,27 +20,45 @@ public:
 		for (int i = 0; i < windowsCount; i++)
 		{
 			windows.push_back(Window::Create(desc));
+			windows[i]->eventHandler = [this, i](Event& e) { OnEventMultiWindow(i, e); };
 		}
 	}
 	void OnUpdate() override {
-		windows.at(0)->PollEvents();
-
 		for (auto it = windows.begin(); it != windows.end();)
 		{
 			const auto& window = *it;
+
+			window->PollEvents();
+
 			if (window->ShouldClose())
 			{
 				openedWindowsCount--;
 				if (openedWindowsCount == 0)
 					Framework::Application::Get().Stop();
 				it = windows.erase(it);
+				continue;
 			}
 			else
 				it++;
+
+			window->DispatchEvents();
 		}
 	}
 	void OnRender() override {}
-	void OnEvent(Framework::Event& e) override {}
+	void OnEvent(Event& e) override {}
+
+	void OnEventMultiWindow(int windowID, Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<Keyboard::Event>([windowID](Keyboard::Event& e) {
+			if (e.GetCode() == FW_KEY_ENTER && e.IsPress())
+			{
+				LOG_INFO("Enter pressed on window {}.", windowID);
+				return true;
+			}
+			return false;
+		});
+	}
 	void OnDetach() noexcept override {}
 
 private:
